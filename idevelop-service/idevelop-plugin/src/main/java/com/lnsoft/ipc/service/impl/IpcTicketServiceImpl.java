317c0acf555ffca01b491ce76380302151db62cc
leo <qduwml@163.com>
2026-04-14 08:46:31 +0800

    修复重复ticket的问题，当ticket已存在时更新记录而不是插入新记录

--- idevelop-service/idevelop-plugin/src/main/java/com/lnsoft/ipc/service/impl/IpcTicketServiceImpl.java
+++ idevelop-service/idevelop-plugin/src/main/java/com/lnsoft/ipc/service/impl/IpcTicketServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lnsoft.ipc.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lnsoft.ipc.entity.IpcTicket;
 import com.lnsoft.ipc.mapper.IpcTicketMapper;
@@ -19,13 +20,26 @@ public class IpcTicketServiceImpl extends ServiceImpl<IpcTicketMapper, IpcTicket
 
     @Override
     public boolean saveTicket(String ticket, String ip) {
-        IpcTicket ipcTicket = new IpcTicket();
-        ipcTicket.setTicket(ticket);
-        ipcTicket.setIp(ip);
-        ipcTicket.setCreateTime(LocalDateTime.now());
-        ipcTicket.setUpdateTime(LocalDateTime.now());
-        ipcTicket.setIsDeleted(0);
-        return this.save(ipcTicket);
+        // 先查询是否存在相同的ticket
+        LambdaQueryWrapper<IpcTicket> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(IpcTicket::getTicket, ticket);
+        IpcTicket existingTicket = this.getOne(wrapper);
+
+        if (existingTicket != null) {
+            // 如果存在，则更新记录
+            existingTicket.setIp(ip);
+            existingTicket.setUpdateTime(LocalDateTime.now());
+            return this.updateById(existingTicket);
+        } else {
+            // 如果不存在，则插入新记录
+            IpcTicket ipcTicket = new IpcTicket();
+            ipcTicket.setTicket(ticket);
+            ipcTicket.setIp(ip);
+            ipcTicket.setCreateTime(LocalDateTime.now());
+            ipcTicket.setUpdateTime(LocalDateTime.now());
+            ipcTicket.setIsDeleted(0);
+            return this.save(ipcTicket);
+        }
     }
 
 }
