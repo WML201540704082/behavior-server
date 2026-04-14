8fea332c61be5f45fc2da5722b8a15a03dd7755b
leo <qduwml@163.com>
2026-04-14 08:53:36 +0800

    修改saveTicket方法，当远程IP相同时替换其ticket

--- idevelop-service/idevelop-plugin/src/main/java/com/lnsoft/ipc/service/impl/IpcTicketServiceImpl.java
+++ idevelop-service/idevelop-plugin/src/main/java/com/lnsoft/ipc/service/impl/IpcTicketServiceImpl.java
@@ -20,25 +20,37 @@ public class IpcTicketServiceImpl extends ServiceImpl<IpcTicketMapper, IpcTicket
 
     @Override
     public boolean saveTicket(String ticket, String ip) {
-        // 先查询是否存在相同的ticket
-        LambdaQueryWrapper<IpcTicket> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(IpcTicket::getTicket, ticket);
-        IpcTicket existingTicket = this.getOne(wrapper);
+        // 先查询是否存在相同的IP
+        LambdaQueryWrapper<IpcTicket> ipWrapper = new LambdaQueryWrapper<>();
+        ipWrapper.eq(IpcTicket::getIp, ip);
+        IpcTicket existingByIp = this.getOne(ipWrapper);
 
-        if (existingTicket != null) {
-            // 如果存在，则更新记录
-            existingTicket.setIp(ip);
-            existingTicket.setUpdateTime(LocalDateTime.now());
-            return this.updateById(existingTicket);
+        if (existingByIp != null) {
+            // 如果存在相同的IP，则更新记录的ticket和updateTime
+            existingByIp.setTicket(ticket);
+            existingByIp.setUpdateTime(LocalDateTime.now());
+            return this.updateById(existingByIp);
         } else {
-            // 如果不存在，则插入新记录
-            IpcTicket ipcTicket = new IpcTicket();
-            ipcTicket.setTicket(ticket);
-            ipcTicket.setIp(ip);
-            ipcTicket.setCreateTime(LocalDateTime.now());
-            ipcTicket.setUpdateTime(LocalDateTime.now());
-            ipcTicket.setIsDeleted(0);
-            return this.save(ipcTicket);
+            // 再查询是否存在相同的ticket
+            LambdaQueryWrapper<IpcTicket> ticketWrapper = new LambdaQueryWrapper<>();
+            ticketWrapper.eq(IpcTicket::getTicket, ticket);
+            IpcTicket existingByTicket = this.getOne(ticketWrapper);
+
+            if (existingByTicket != null) {
+                // 如果存在相同的ticket，则更新记录的ip和updateTime
+                existingByTicket.setIp(ip);
+                existingByTicket.setUpdateTime(LocalDateTime.now());
+                return this.updateById(existingByTicket);
+            } else {
+                // 如果都不存在，则插入新记录
+                IpcTicket ipcTicket = new IpcTicket();
+                ipcTicket.setTicket(ticket);
+                ipcTicket.setIp(ip);
+                ipcTicket.setCreateTime(LocalDateTime.now());
+                ipcTicket.setUpdateTime(LocalDateTime.now());
+                ipcTicket.setIsDeleted(0);
+                return this.save(ipcTicket);
+            }
         }
     }
 
